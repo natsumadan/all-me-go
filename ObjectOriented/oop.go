448b498ccfec1bb3e09f1c21186ffa0c10fd5b53
natsumadan <madan.mohit209@gmail.com>
2021-02-16 05:55:35 +1000

    update

--- ObjectOriented/oop.go
+++ ObjectOriented/oop.go
@@ -1,5 +1,37 @@
 package main
 
+
+import (
+	"fmt"
+)
+
+
+// Struct 
+type Square struct {
+	Length int
+	Centre float64
+
+}
+
+type Point struct {
+	X int
+	Y int
+}
+
+func (p *Point) Move(dx int, dx int) {
+	p.X += dx
+	p.Y += dy
+}
+
+func Area() int {
+	
+
+}
+
+func NewSquare(x int , y int,, length int)(*Square, error) {
+
+}
+
 func main() {
 
 }
